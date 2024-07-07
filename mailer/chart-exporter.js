9e9e514a3a3cf9f4a4e54c284f674c64553ebc8d
AyanS1912 <92726765+AyanS1912@users.noreply.github.com>
2024-07-07 19:32:57 +0530

    Puppeteer used for screenshots

--- mailer/chart-exporter.js
+++ mailer/chart-exporter.js
@@ -1,6 +1,8 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const puppeteer = require("puppeteer");
 
 const cors = require("cors");
 const app = express();
@@ -9,15 +11,131 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 
-app.post("/send-email", (req, res) => {
-  console.log("Request recieved");
-  const { images } = req.body;
 
-  let htmlContent = `
+//Send Email call1
+// app.post("/send-email", (req, res) => {
+//   const { images } = req.body;
+
+//   let htmlContent = `
+//       <html>
+//         <head>
+//           <style>
+//           /*
+//             .flex-container {
+//               display: flex;
+//               flex-wrap: wrap;
+//               justify-content: center;
+//               width: 100vw;
+//               position : fixed;
+//             }
+
+//             .scroll-container {
+//               max-height: 400px;
+//               overflow: auto;
+//               scrollbar-width: thin;
+//               scrollbar-color: #888 #f0f0f0;
+//               padding : 10px;
+//               margin : 10px;
+//               box-sizing : border-box;
+//               justify-content : space-around;
+//               align-content : center;
+//             }
+
+//             .scroll-container img {
+//               max-width: 100%;
+//               max-height: 100%;
+//               display: block;
+//             }
+
+//             .scroll-container::-webkit-scrollbar {
+//               width: 6px;
+//               height: 6px;
+//             }
+//             .scroll-container::-webkit-scrollbar-thumb {
+//               background-color: #888;
+//               border-radius: 3px;
+//             }
+//             .scroll-container::-webkit-scrollbar-track {
+//               background-color: #f0f0f0;
+//             }
+
+//           */
+//           </style>
+//         </head>
+//         <body>
+//         <div class="flex-container">`;
+
+//   let attachments = images.map((image, index) => {
+//     console.log(image)
+//     const imageBuffer = Buffer.from(image.split(",")[1], "base64");
+//     htmlContent += `
+//       <div class="scroll-container">
+//         <img src="cid:highchartImage${index}" alt="Highchart${index}"/>
+//       </div>
+//       `;
+//     return {
+//       filename: `highchart${index}.png`,
+//       content: imageBuffer,
+//       cid: `highchartImage${index}`,
+//     };
+//   });
+
+//   htmlContent += ` </div>
+//     </body>
+//   </html>`;
+
+//   console.log("htmlContent", htmlContent);
+
+//   let transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: "ayansheikh.cse@gmail.com",
+//       pass: "gwmt emxb xwhu frza",
+//     },
+//   });
+
+//   let mailOptions = {
+//     form: "ayansheikh.cse@gmail.com",
+//     to: "ayan.sheikh@innovapptive.com",
+//     subject: "Send Widget -Proper visible Chart",
+//     html: htmlContent,
+//     attachments: attachments,
+//   };
+//   console.log(mailOptions)
+
+//   transporter.sendMail(mailOptions, (err, info) => {
+//     if (err) {
+//       return res.status(500).send(err.toString());
+//     }
+//     res.status(200).send("Mail Sent" + info.response);
+//   });
+// });
+
+app.post("/send-email", async(req, res) => {
+  try{
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    await page.goto('http://localhost:4200');
+
+    const chartContainers = await page.$$('#container');
+
+    const getScreenshots = []
+
+    for (let i = 0; i < chartContainers.length; i++) {
+      const chart = chartContainers[i];
+      const screenshot = await chart.screenshot({ encoding: 'base64' });
+      getScreenshots.push(`data:image/png;base64,${screenshot}`);
+    }
+
+    await browser.close()
+
+
+      let htmlContent = `
       <html>
         <head>
           <style>
-
+          /*
             .flex-container {
               display: flex;
               flex-wrap: wrap;
@@ -56,13 +174,15 @@ app.post("/send-email", (req, res) => {
               background-color: #f0f0f0;
             }
 
-           
+          */
           </style>
         </head>
         <body>
         <div class="flex-container">`;
-
-  let attachments = images.map((image, index) => {
+  
+  console.log("SCe=reen",getScreenshots)
+  let attachments = getScreenshots.map((image, index) => {
+    console.log(image)
     const imageBuffer = Buffer.from(image.split(",")[1], "base64");
     htmlContent += `
       <div class="scroll-container">
@@ -97,6 +217,7 @@ app.post("/send-email", (req, res) => {
     html: htmlContent,
     attachments: attachments,
   };
+  console.log(mailOptions)
 
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) {
@@ -104,37 +225,14 @@ app.post("/send-email", (req, res) => {
     }
     res.status(200).send("Mail Sent" + info.response);
   });
-});
+
+  }
+  catch(err){
+    console.error("Error capturing screenshot or sending email:", err);
+    res.status(500).send("Internal server error");
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
-
-// async function sendMail() {
-//   let transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: "ayansheikh.cse@gmail.com",
-//       pass: "gwmt emxb xwhu frza",
-//     },
-//   });
-
-//   let htmlContent = fs.readFileSync("./emailTemplate.html");
-
-//   let mailOptions = {
-//     from: "ayansheikh.cse@gmail.com",
-//     to: "ayan.sheikh@innovapptive.com",
-//     subject: "Widget Mail",
-//     // html: `
-//     // <div style="overflow: auto; width: 100%; height: 500px;">
-//     //     ${svg}
-//     // </div>
-//     // `,
-//     html: htmlContent,
-//   };
-
-//   let info = await transporter.sendMail(mailOptions);
-//   console.log("Message sent: %s", info.messageId);
-// }
-
-// sendMail().catch(console.error);
--- widget-mailer/src/app/widgets/widgets.component.ts
+++ widget-mailer/src/app/widgets/widgets.component.ts
@@ -6,10 +6,13 @@ import {
   ViewChild,
   ViewChildren,
 } from "@angular/core";
-// import * as Highcharts from "highcharts";
 import html2canvas from "html2canvas";
 import axios from "axios";
-import * as Highcharts from "highcharts/highstock";
+// import * as Highcharts from "highcharts/highstock";
+import * as Highcharts from "highcharts"
+import HC_exporting from "highcharts/modules/exporting";
+import HC_Data from "highcharts/modules/export-data";
+import Accessbility from "highcharts/modules/accessibility";
 
 @Component({
   selector: "app-widgets",
@@ -17,8 +20,12 @@ import * as Highcharts from "highcharts/highstock";
   styleUrl: "./widgets.component.scss",
 })
 export class WidgetsComponent implements OnInit {
-  @ViewChildren("chartContainer1, chartContainer2, chartContainer3")
-  chartContainers!: QueryList<ElementRef>;
+  // @ViewChildren("chartContainer1, chartContainer1, chartContainer3")
+  // chartContainers!: QueryList<ElementRef>;
+
+  // @ViewChild("chartContainer1", { static: false }) chartContainer1: any;
+  // @ViewChild("chartContainer2", { static: false }) chartContainer2: any;
+  // @ViewChild("chartContainer3", { static: false }) chartContainer3: any;
 
   isHighcharts: boolean = false;
   ngOnInit(): void {
@@ -65,10 +72,9 @@ export class WidgetsComponent implements OnInit {
         load: function () {
           const chart = this;
           const numCategories = chart.series[0].data.length;
-
           // Set the max dynamically based on number of categories
-          if (numCategories > 6) {
-            chart.xAxis[0].setExtremes(0, 6);
+          if (numCategories > 5) {
+            chart.xAxis[0].setExtremes(0, 5);
             chart.xAxis[0].update({
               scrollbar: {
                 enabled: true,
@@ -83,6 +89,10 @@ export class WidgetsComponent implements OnInit {
           }
         },
       },
+      // scrollablePlotArea: {
+      //   minHeight: 800,
+      //   minWidth: 800,
+      // },
     },
     title: {
       text: "Most popular ideas by April 2016",
@@ -99,6 +109,10 @@ export class WidgetsComponent implements OnInit {
       min: 0,
       max: 4,
       tickLength: 0,
+      // scrollbar: {
+      //   enabled: true,
+      //   showFull: false,
+      // },
     },
     yAxis: {
       min: 0,
@@ -144,27 +158,29 @@ export class WidgetsComponent implements OnInit {
       type: "bar",
       marginLeft: 150,
       events: {
-        load: function () {
-          const chart = this;
-          const numCategories = chart.series[0].data.length;
-
-          // Set the max dynamically based on number of categories
-          if (numCategories > 6) {
-            chart.xAxis[0].setExtremes(0, 6);
-            chart.xAxis[0].update({
-              scrollbar: {
-                enabled: true,
-              },
-            });
-          } else {
-            chart.xAxis[0].update({
-              scrollbar: {
-                enabled: false,
-              },
-            });
-          }
-        },
+        // load: function () {
+        //   const chart = this;
+        //   const numCategories = chart.series[0].data.length;
+        //   // Set the max dynamically based on number of categories
+        //   if (numCategories > 5) {
+        //     chart.xAxis[0].setExtremes(0, 5);
+        //     chart.xAxis[0].update({
+        //       scrollbar: {
+        //         enabled: true,
+        //       },
+        //     });
+        //   } else {
+        //     chart.xAxis[0].update({
+        //       scrollbar: {
+        //         enabled: false,
+        //       },
+        //     });
+        //   }
+        // },
       },
+      scrollablePlotArea:{
+        minHeight: 400,
+      }
     },
     title: {
       text: "Most popular ideas by April 2016",
@@ -180,9 +196,10 @@ export class WidgetsComponent implements OnInit {
       },
       min: 0,
       max: 4,
-      scrollbar: {
-        enabled: true,
-      },
+      // scrollbar: {
+      //   enabled: true,
+      //   showFull: false
+      // },
       tickLength: 0,
     },
     yAxis: {
@@ -232,6 +249,26 @@ export class WidgetsComponent implements OnInit {
           ["Draw lines over a stock chart, for analysis purpose", 118],
           ["Data module for database tables", 118],
           ["Draggable points", 117],
+          ["Gantt chart", 1000],
+          ["Autocalculation and plotting of trend lines", 575],
+          ["Allow navigator to have multiple data series", 523],
+          ["Implement dynamic font size", 427],
+          ["Multiple axis alignment control", 399],
+          ["Stacked area (spline etc) in irregular datetime series", 309],
+          ["Adapt chart height to legend height", 278],
+          ["Export charts in excel sheet", 239],
+          ["Toggle legend box", 235],
+          ["Venn Diagram", 203],
+          ["Add ability to change Rangeselector position", 182],
+          ["Draggable legend box", 157],
+          ["Sankey Diagram", 149],
+          ["Add Navigation bar for Y-Axis in Highcharts Stock", 144],
+          ["Grouped x-axis", 143],
+          ["ReactJS plugin", 137],
+          ["3D surface charts", 134],
+          ["Draw lines over a stock chart, for analysis purpose", 118],
+          ["Data module for database tables", 118],
+          ["Draggable points", 117],
         ],
       },
     ],
@@ -239,40 +276,46 @@ export class WidgetsComponent implements OnInit {
 
   constructor() {}
 
+  //send mail capture screen shots
+  // sendEmail() {
+  //   const chartElements = document.querySelectorAll("#container");
+  //   const imagePromises = Array.from(chartElements).map((element) => {
+  //     const takeOriginalHeight = (element as HTMLElement).style.height;
+  //     const takeOriginalWidth = (element as HTMLElement).style.width;
+
+  //     (element as HTMLElement).style.height = "auto";
+  //     (element as HTMLElement).style.width = "auto";
+
+  //     return html2canvas(element as HTMLElement).then((canvas) => {
+  //       (element as HTMLElement).style.height = takeOriginalHeight;
+  //       (element as HTMLElement).style.width = takeOriginalWidth;
+  //       return canvas.toDataURL("image/png");
+  //     });
+  //   });
+
+  //   Promise.all(imagePromises).then((images) => {
+  //     axios
+  //       .post("http://localhost:3000/send-email", { images })
+  //       .then((response) => {
+  //         console.log("Email sent successfully");
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error sending email", error);
+  //       });
+  //   });
+  // }
+
+  //call to pupetter to make screen shot in headless browserr.
   sendEmail() {
-    // const imagePromises = chartElements.map((element) => {
-    //   // const takeOriginalHeight = (element as HTMLElement).style.height;
-    //   // const takeOriginalWidth = (element as HTMLElement).style.width;
-
-    //   // (element as HTMLElement).style.height = "auto";
-    //   // (element as HTMLElement).style.width = "auto";
-
-    //   return html2canvas(element as HTMLElement).then((canvas) => {
-    //     // (element as HTMLElement).style.height = takeOriginalHeight;
-    //     // (element as HTMLElement).style.width = takeOriginalWidth;
-    //     return canvas.toDataURL("image/png");
-    //   });
-    // });
-
-    const imagePromises = this.chartContainers.map(async (container) => {
-      const canvas = await html2canvas(container.nativeElement, {
-        scrollX: scrollX,
-        scrollY: scrollY,
-        width: container.nativeElement.scrollWidth,
-        height: container.nativeElement.scrollHeight,
+    axios
+      .post("http://localhost:3000/send-email", {})
+      .then((response) => {
+        console.log("Email sent successfully");
+      })
+      .catch((error) => {
+        console.error("Error sending email", error);
       });
-      return canvas.toDataURL("image/png");
-    });
-
-    // Promise.all(imagePromises).then((images) => {
-    //   axios
-    //     .post("http://localhost:3000/send-email", { images })
-    //     .then((response) => {
-    //       console.log("Email sent successfully");
-    //     })
-    //     .catch((error) => {
-    //       console.error("Error sending email", error);
-    //     });
-    // });
   }
+
+  
 }
